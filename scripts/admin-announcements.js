abf2554a932410f159cc89761fdd9fcc2ed1b6c4
a7-creations <238r1a05@gmail.com>
2025-04-23 23:50:04 +0530

    Initial commit

--- scripts/admin-announcements.js
+++ scripts/admin-announcements.js
@@ -5,16 +5,14 @@ import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/
 
 // Your Firebase configuration object
 const firebaseConfig = {
-    apiKey: "AIzaSyCJ9vaClojHn_IIY2YC8XOBlY7JtHmt5vM",
-    authDomain: "check-e4504.firebaseapp.com",
-    databaseURL: "https://check-e4504-default-rtdb.firebaseio.com",
-    projectId: "check-e4504",
-    storageBucket: "check-e4504.appspot.com",  // ✅ Fixed storageBucket
-    messagingSenderId: "753492063096",
-    appId: "1:753492063096:web:e499ee4f211f8f9f82ce8d",
-    measurementId: "G-5TRQEHS5WM"
-};
-
+    apiKey: "AIzaSyByiV9J9UFJyq4YZ7lsx4UOsb91K255HZE",
+    authDomain: "cmrec-nexus.firebaseapp.com",
+    projectId: "cmrec-nexus",
+    storageBucket: "cmrec-nexus.firebasestorage.app",
+    messagingSenderId: "1037530313729",
+    appId: "1:1037530313729:web:8c02807518980435517c00",
+    measurementId: "G-DKSXWND59G"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
